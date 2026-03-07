0d1f4e22644fce69f75ca574a44cff68a533a60a
Srojasal <127476251+Srojasal@users.noreply.github.com>
2026-03-07 16:43:58 -0600

    TareaClase6

---
+++ TareaClase6/main.js
@@ -0,0 +1,23 @@
+
+//Ejercicio 1
+//Calcular si un numero esta entre 10 y 20 o entre 40 y 60
+
+let number = 55
+
+true 
+false 
+
+let result = (number >= 10 && number <= 20) || (number >= 40 && number <= 60)
+console.log("El calculo de rangos para el numero " + number + " es: " + result) 
+
+
+//Ejercicio 2
+//Calcular la edad humana de un perrito de acuerdo a su edad.
+
+let dogAge = 6
+let humanAge = 20 + (dogAge - 2) * 4;
+console.log("El perrito de " + dogAge + " años tendría " + humanAge + " años si fuera humano")
+
+
+
+
